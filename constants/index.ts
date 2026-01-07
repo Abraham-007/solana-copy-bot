c7d13a7d30c06b090c716aeabf8dca6e82ead9d9
Abraham <137196067+Abraham-007@users.noreply.github.com>
2026-01-07 21:56:00 +0800

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"; 
 dotenv.config();
 export const RPC_ENDPOINT=process.env.RPC_ENDPOINT;
 export const RPC_WEBSOCKET_ENDPOINT=process.env.RPC_WEBSOCKET_ENDPOINT;
